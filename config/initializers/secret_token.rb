eaccdfd5eb0b6a9ab2b5ba8955d6ed7fba739a16
Annie Chen <annieche@andrew.cmu.edu>
2014-11-13 12:59:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NovoEDChallenge::Application.config.secret_key_base = '0c8718448276a82f85e6ad9cedce66462e199a447ee5d49821dfc745cb87e7972494b776a0f82be4fcfa0f5f3440aaeecc2c5aa19a6430f534d474ea10394de0'
